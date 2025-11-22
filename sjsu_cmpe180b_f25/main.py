e2502485eb302d18f4512a34733b2ff83b19bbe1
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-11-21 17:45:28 -0800

    log `log_level` in main

--- sjsu_cmpe180b_f25/main.py
+++ sjsu_cmpe180b_f25/main.py
@@ -12,9 +12,7 @@ def main(argv: Sequence[str] | None = None) -> None:
         format="[%(levelname)s][%(asctime)s][%(name)s] %(message)s",
     )
 
-    logging.getLogger(__name__).info("hello world")
-
-    logging.getLogger(__name__).info(f"args {cli_args.database_url}")
+    logging.getLogger(__name__).info(f"Utilizing log level '{cli_args.log_level}'")
 
 
 def cli(argv: Sequence[str] | None = None) -> None:
