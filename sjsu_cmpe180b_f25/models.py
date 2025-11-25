cbcfb52e32db600e9efd174fca8042899860a116
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-11-24 22:45:05 -0800

    add models

--- sjsu_cmpe180b_f25/models.py
+++ sjsu_cmpe180b_f25/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -5,7 +6,38 @@ class Base(DeclarativeBase):
     pass
 
 
-class ExampleModel(Base):
-    __tablename__ = "example_table"
+class Author(Base):
+    __tablename__ = "authors"
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+
+
+class Book(Base):
+    __tablename__ = "books"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    author_id: Mapped[int] = mapped_column(ForeignKey("authors.id"), nullable=False)
+
+
+class Member(Base):
+    __tablename__ = "members"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
+
+
+class Loan(Base):
+    __tablename__ = "loans"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    book_id: Mapped[int] = mapped_column(ForeignKey("books.id"), nullable=False)
+    member_id: Mapped[int] = mapped_column(ForeignKey("members.id"), nullable=False)
+    loan_date: Mapped[str] = mapped_column(nullable=False)
+    return_date: Mapped[str] = mapped_column(nullable=True)
+
+
+class Fine(Base):
+    __tablename__ = "fines"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    member_id: Mapped[int] = mapped_column(ForeignKey("members.id"), nullable=False)
+    amount: Mapped[float] = mapped_column(nullable=False)
+    paid: Mapped[bool] = mapped_column(nullable=False, default=False)
