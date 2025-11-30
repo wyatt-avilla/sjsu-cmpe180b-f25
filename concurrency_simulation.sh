0e5dac74fb398e4e006ce8bba441e3095e65cc5f
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-11-29 18:07:23 -0800

    add `--verbose` and parallel execution logic

--- concurrency_simulation.sh
+++ concurrency_simulation.sh
@@ -7,6 +7,7 @@ Usage: $(basename "$0") -t <integer> -- [options...]
 Options:
   -t <integer>    Specify number of concurrent calls to databse CLI (required)
   --              Separator before database CLI options
+  --versbose      Don't suppress output from database CLI
   --help          Show this help message
 
 Example:
@@ -22,11 +23,16 @@ fi
 
 t_value=""
 rest_args=()
+verbose=0
 
 while [[ $# -gt 0 ]]; do
 	case $1 in
 	--help)
 		print_help
+		;;
+	--verbose)
+		verbose=1
+		shift
 		;;
 	-t)
 		t_value="$2"
@@ -51,5 +57,26 @@ if [[ -z "${t_value}" ]]; then
 	exit 1
 fi
 
-echo "t_value: ${t_value}"
-echo "rest_args:" "${rest_args[@]}"
+for i in $(seq 1 "${t_value}"); do
+	(
+		echo "[${BASHPID}] Starting concurrent call ${i}"
+
+		if [[ ${verbose} -eq 1 ]]; then
+			python -m sjsu_cmpe180b_f25.main "${rest_args[@]}"
+			exit_status=$?
+		else
+			python -m sjsu_cmpe180b_f25.main "${rest_args[@]}" >/dev/null 2>&1
+			exit_status=$?
+		fi
+
+		echo "[${BASHPID}] Completed concurrent call ${i} with exit status ${exit_status}"
+		if [[ ${exit_status} -eq 0 ]]; then
+			echo -e "[${BASHPID}][\e[32mSUCCEEDED\e[0m]"
+		else
+			echo -e "[${BASHPID}][\e[31mFAILED\e[0m]"
+		fi
+	) &
+done
+
+wait
+echo "All concurrent calls completed."
