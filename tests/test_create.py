031128ef13917be3bb215ba674a752bc62f1db13
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-11-29 22:32:46 -0800

    add test create book author

--- tests/test_create.py
+++ tests/test_create.py
@@ -36,3 +36,27 @@ async def test_create_book(test_client: Client) -> None:
     )
 
     assert book is not None
+
+
+@pytest.mark.asyncio
+async def test_create_book_author(test_client: Client) -> None:
+    """Test that a book with an author can be created successfully."""
+
+    author = await test_client.create_author(id=1, name="Benjamin Reichwald")
+    assert author is not None
+
+    book = await test_client.create_book(
+        book_id=1,
+        title="Introduction to Software Engineering",
+        isbn="978-3-16-148410-0",
+        published_year=2024,
+        genre="Education",
+    )
+    assert book is not None
+
+    book_author = await test_client.create_book_author(book_id=1, author_id=1)
+
+    assert book_author is not None
+
+    assert book_author.book_id == book.book_id
+    assert book_author.author_id == author.author_id
