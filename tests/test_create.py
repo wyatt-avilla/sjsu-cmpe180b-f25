065183f5a1fcbe7debf831fdfa43da09576fc195
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-11-29 22:28:43 -0800

    test create author with duplicates

--- tests/test_create.py
+++ tests/test_create.py
@@ -10,3 +10,14 @@ async def test_create_author(test_client: Client) -> None:
     author = await test_client.create_author(id=1, name="Benjamin Reichwald")
 
     assert author is not None
+
+
+@pytest.mark.asyncio
+async def test_create_author_duplicate_id(test_client: Client) -> None:
+    """Test that creating an author with a duplicate ID fails."""
+
+    author1 = await test_client.create_author(id=1, name="Benjamin Reichwald")
+    author2 = await test_client.create_author(id=1, name="Zak Arogundade")
+
+    assert author1 is not None
+    assert author2 is None
