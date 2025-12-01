43b66a5ad78978352e3b782c2dc3ea15896194f5
Wyatt Avilla <wyattmurphy1@gmail.com>
2025-11-30 17:55:55 -0800

    use data subset in populate db test

--- tests/test_populate_db.py
+++ tests/test_populate_db.py
@@ -8,4 +8,11 @@ from sjsu_cmpe180b_f25.population import populate_db
 async def test_populate_db(test_client: Client) -> None:
     """Test requesting a loan successfully."""
 
-    await populate_db(test_client)
+    await populate_db(
+        test_client,
+        num_authors=10,
+        num_books=10,
+        num_members=10,
+        copies_per_book=2,
+        num_loans=10,
+    )
